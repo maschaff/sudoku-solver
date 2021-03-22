1b78bae56cbb6e7f75044375bcf99cec4f266f13
maschaff <schaffer.mb@gmail.com>
2021-03-22 12:16:26 -0500

    Updated sudoku conversion to work properly and more efficiently than in previous attempts. Removed redundant code.

--- src/services/sudoku.js
+++ src/services/sudoku.js
@@ -2,65 +2,56 @@ import DancingLinks from './dancingLinks'
 
 export default {
 
-    // eslint-disable-next-line no-unused-vars
     solve(sudoku) {
-
-        let binaryMatrix = this.generateSudokuToBinaryMatrix(9);
+        let binaryMatrix = this.convertSudokuToBinaryMatrix(sudoku);
         this.printBinaryMatrix(binaryMatrix);
-
         return DancingLinks.solveWithDancingLinks(binaryMatrix);
     },
 
-    /*
-    * Each row represents a number in a given position on the grid i.e.
-    * 4 constraints are represented in each row by nxn columns
-    *
-    * 1. One number per cell
-    * 2. One instance of a number per row
-    * 3. One instance of a number per col
-    * 4. One instance of a number per region
-    */
-    generateSudokuToBinaryMatrix(n) {
-        let result = this.initEmptyBinaryMatrix(n);
-        let nxn = Math.pow(n, 2);
+    // Builds a binary matrix representation of a given sudoku puzzle which can be solved as an exact cover problem
+    convertSudokuToBinaryMatrix(sudoku) {
+        let n = sudoku.length;
+        let result = [];
 
-        // generate complete binary matrix for a sudoku of a given size n
-        for (let i = 0; i < n; i++) {
-            for (let j = 0; j < n; j++) {
-                for (let k = 0; k < n; k++) {
-                    // (candidate, row, col) - > (k, i, j)
-                    let rowIndex = (i * nxn) + (j * n) + k;
-                    let region_offset_row = Math.floor( i/Math.sqrt(n) ) * Math.sqrt(n) * n;
-                    let region_offset_col = Math.floor( j/Math.sqrt(n) ) * n;
-
-                    let constraint1 = (i * n) + j ;
-                    let constraint2 = (nxn) + (i * n) + k;
-                    let constraint3 = (nxn * 2) + (j * n) + k;
-                    let constraint4 = (nxn * 3) + region_offset_row + region_offset_col + k;
-
-                    result[rowIndex][constraint1] = 1;
-                    result[rowIndex][constraint2] = 1;
-                    result[rowIndex][constraint3] = 1;
-                    result[rowIndex][constraint4] = 1;
+        for (let row = 0; row < n; row++) {
+            for (let col = 0; col < n; col++) {
+                if (sudoku[row][col] != null) {
+                    result.push(this.buildRow(sudoku[row][col] - 1, row, col, n));
+                } else {
+                    for (let cand = 0; cand < n; cand++) {
+                        result.push(this.buildRow(cand, row, col, n));
+                    }
                 }
             }
         }
-
         return result;
     },
 
-    initEmptyBinaryMatrix(n) {
-        let result = [];
-        let numRows = n * n * n;
+    /*  Builds a row representing a candidate in a given position on a sudoku board given the 4 sudoku constraints
+     *  1. One number per cell
+     *  2. One instance of a number per row
+     *  3. One instance of a number per col
+     *  4. One instance of a number per region
+     */
+    buildRow(cand, row, col, n) {
         let numCols = n * n * 4;
+        let result = [];
 
-        for(let i = 0; i < numRows; i++) {
-            result[i] = [];
-            for(let j = 0; j < numCols; j++) {
-                result[i].push(0);
+        let region_offset_row = Math.floor(row / Math.sqrt(n)) * Math.sqrt(n) * n;
+        let region_offset_col = Math.floor(col / Math.sqrt(n)) * n;
+
+        let constraint1 = (row * n) + col;
+        let constraint2 = (n * n) + (row * n) + cand;
+        let constraint3 = (n * n * 2) + (col * n) + cand;
+        let constraint4 = (n * n * 3) + region_offset_row + region_offset_col + cand;
+
+        for (let i = 0; i < numCols; i++) {
+            if (i === constraint1 || i === constraint2 || i === constraint3 || i === constraint4) {
+                result.push(1);
+            } else {
+                result.push(0);
             }
         }
-
         return result;
     },
 
@@ -68,7 +59,7 @@ export default {
         let result = ""
         for (let i = 0; i < matrix.length; i++) {
             for (let j = 0; j < matrix[i].length; j++) {
-                if(matrix[i][j] > 0) {
+                if (matrix[i][j] > 0) {
                     result += matrix[i][j] + " ";
                 } else {
                     result += "  ";
